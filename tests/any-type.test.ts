9672f6a974fc49f551085e97783ea220e3f486ce
Sultan Room <sultanroomcode@gmail.com>
2024-06-05 22:39:34 +0700

    add it test

--- tests/any-type.test.ts
+++ tests/any-type.test.ts
@@ -1,13 +1,15 @@
 describe('check if any', function(){
-    // sebaiknya jangan gegabah. jangan gunakan any terlalu sering, anda menggunakan typescript untuk terjaganya script.
-    const person: any = {
-        id: 1, 
-        name: "Agus Sutarom",
-        age: 30
-    };
+    it('harus di deklarasikan', function(){
+        // sebaiknya jangan gegabah. jangan gunakan any terlalu sering, anda menggunakan typescript untuk terjaganya script.
+        const person: any = {
+            id: 1, 
+            name: "Agus Sutarom",
+            age: 30
+        };
 
-    person.age = 33;
-    person.address = 'Jl. Temandang Merakurak';
+        person.age = 33;
+        person.address = 'Jl. Temandang Merakurak';
 
-    console.info(person);
+        console.info(person);
+    });
 });
