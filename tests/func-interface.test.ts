c94a94f256306f205b5c8d18d1039a556adf23de
Sultan Room <sultanroomcode@gmail.com>
2024-06-08 20:54:46 +0700

    add identation

--- tests/func-interface.test.ts
+++ tests/func-interface.test.ts
@@ -1,14 +1,14 @@
-describe('should interface function', function() {
-  it('interface function', function() {
+describe('should interface function', function () {
+  it('interface function', function () {
     interface functionForYou {
-        (val1: number, val2:number): number;
+      (val1: number, val2: number): number;
     }
 
-    const add: functionForYou = (val1: number, val2:number): number => {
-        return val1 + val2;
+    const add: functionForYou = (val1: number, val2: number): number => {
+      return val1 + val2;
     }
 
-    expect(add(2,3)).toBe(5);
-    expect(add(5,3)).toBe(8);
+    expect(add(2, 3)).toBe(5);
+    expect(add(5, 3)).toBe(8);
   });
 });
