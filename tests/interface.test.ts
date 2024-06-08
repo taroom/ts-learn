446120dc640fe6fe68dc0f4075c0446f5d8b58a1
Sultan Room <sultanroomcode@gmail.com>
2024-06-08 18:26:50 +0700

    add seller interface

---
+++ tests/interface.test.ts
@@ -0,0 +1,16 @@
+import { Seller } from "../src/seller-interface";
+
+describe('must interface', function() {
+  it('seller interface', function() {
+    const seller:Seller = {
+        id: 1,
+        name: 'Agus Sutarom'
+    }
+
+    const seller2:Seller = {
+        id: 2,
+        name: 'Khoirul Abdul',
+        address: 'Kembangbilo'
+    }
+  });
+});
